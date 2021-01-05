3ae465c87c7990d939154e442dccf154c547c3a1
Becki Wordsworth <r.a.wordsworth@gmail.com>
2021-01-05 22:07:53 +0100

    Hooking up the routes

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,15 @@ connection.once("open", () => {
 
 const userRouter = require("./routes/user");
 const topicRouter = require("./routes/topic");
+const questionRouter = require("./routes/question");
+const resultRouter = require("./routes/result");
+const sessionRouter = require("./routes/sessions");
 
 app.use("/users", userRouter);
 app.use("/topics", topicRouter);
+app.use("/questions", questionRouter);
+app.use("/results", resultRouter);
+app.use("/sessions", sessionRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
